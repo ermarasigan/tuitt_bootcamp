00afadbce3ed59c8c35bed914c8824c3297f017f
ermarasigan <ermarasigan@outlook.com>
2017-06-21 12:00:29 +0800

    backup as of June 21

---
+++ week6_db/0621/imdb.sql
@@ -0,0 +1,65 @@
+create table  (
+    id int not null auto_increment,
+     varchar(255) not null,
+    primary key(id),
+    foreign key() references ()
+);
+
+Movies
+title
+year
+length
+genre
+studioName
+producerCertificateNumber
+
+create table  (
+    id int not null auto_increment,
+     varchar(255) not null,
+    primary key(id),
+    foreign key() references ()
+);
+
+StarsIn
+movieTitle
+movieYear
+starName
+
+create table  (
+    id int not null auto_increment,
+     varchar(255) not null,
+    primary key(id),
+    foreign key() references ()
+);
+
+MovieStar
+name
+address
+gender
+
+
+create table  (
+    id int not null auto_increment,
+     varchar(255) not null,
+    primary key(id),
+    foreign key() references ()
+);
+
+MovieExec
+name
+address
+certificateNumber
+netWorth
+
+
+create table  (
+    id int not null auto_increment,
+     varchar(255) not null,
+    primary key(id),
+    foreign key() references ()
+);
+
+Studio
+name
+address
+presidentName
