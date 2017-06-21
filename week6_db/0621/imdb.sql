3883a915f65267b82eb47946fc485a8626e7edd6
ermarasigan <ermarasigan@outlook.com>
2017-06-21 16:54:50 +0800

    backup as of June 21

--- week6_db/0621/imdb.sql
+++ week6_db/0621/imdb.sql
@@ -1,41 +1,17 @@
-create table  (
+
+create table MovieStar (
     id int not null auto_increment,
-     varchar(255) not null,
+    name varchar(255) not null,
+    name varchar(255) not null,
     primary key(id),
     foreign key() references ()
 );
 
-Movies
-title
-year
-length
-genre
-studioName
-producerCertificateNumber
 
-create table  (
-    id int not null auto_increment,
-     varchar(255) not null,
-    primary key(id),
-    foreign key() references ()
-);
 
-StarsIn
-movieTitle
-movieYear
-starName
-
-create table  (
-    id int not null auto_increment,
-     varchar(255) not null,
-    primary key(id),
-    foreign key() references ()
-);
-
-MovieStar
-name
 address
 gender
+
 
 
 create table  (
@@ -63,3 +39,51 @@ Studio
 name
 address
 presidentName
+
+
+
+
+
+create table Movies (
+    id int not null auto_increment,
+    title varchar(255) not null,
+    year Year(4) not null,
+    length int not null,
+    genre varchar(255) not null,
+    studioName varchar(255) not null,
+    producerCertificateNumber varchar(255) not null
+    primary key(id),
+    foreign key(studioName) references Studio(name)
+    	on update cascade
+    	on delete set null
+    foreign key(producerCertificateNumber) references MovieExec(certificateNumber)
+    	on update cascade
+    	on delete set null
+);
+
+
+create table StarsIn (
+    id int not null auto_increment,
+    movieTitle varchar(255) not null,
+    movieYear Year(4) not null,
+    starName varchar(255) not null
+    primary key(id),
+    foreign key(movieTitle) references Movies(title)
+    	on update cascade
+    	on delete set null
+    foreign key(movieYear) references Movies(year)
+    	on update cascade
+    	on delete set null
+    foreign key(starName) references MovieStar(name)
+    	on update cascade
+    	on delete set null
+);
+
+
+
+
+
+
+
+
+
